117e619621eb30912bfbb4891a79e5e1f493a52b
p.andhika <p.andhika@op.iitg.ac.in>
2025-08-18 15:04:26 +0700

    feat(auth): add forgot password route and UI components

--- src/routes/(auth)/signin.tsx
+++ src/routes/(auth)/signin.tsx
@@ -1,6 +1,16 @@
 import supabase from "@/lib/supabase/supabase-client";
-import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import { createFileRoute, Link, useNavigate } from "@tanstack/react-router";
 import type { FormEvent } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 
 export const Route = createFileRoute("/(auth)/signin")({
   component: RouteComponent,
@@ -33,36 +43,70 @@ function RouteComponent() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="grid gap-3">
-      <label htmlFor="email">Email</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        placeholder="name@example.com"
-        required
-      />
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <Card>
+          <CardHeader>
+            <CardTitle>Login to your account</CardTitle>
+            <CardDescription>
+              Enter your email below to login to your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form onSubmit={handleSubmit}>
+              <div className="flex flex-col gap-6">
+                <div className="grid gap-3">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    name="email"
+                    type="email"
+                    placeholder="name@example.com"
+                    required
+                  />
+                </div>
 
-      <label htmlFor="password">Password</label>
-      <input
-        id="password"
-        name="password"
-        type="password"
-        placeholder="********"
-        required
-      />
+                <div className="grid gap-3">
+                  <div className="flex items-center">
+                    <Label htmlFor="password">Password</Label>
+                    <Link
+                      to="/forgot-password"
+                      className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                    >
+                      Forgot your password?
+                    </Link>
+                  </div>
 
-      <button className="mt-2" type="submit">
-        Login
-      </button>
+                  <Input
+                    id="password"
+                    name="password"
+                    type="password"
+                    placeholder="********"
+                    required
+                  />
+                </div>
 
-      <button
-        onClick={() => {
-          navigate({ to: "/signup" });
-        }}
-      >
-        Signup
-      </button>
-    </form>
+                <div className="flex flex-col gap-3">
+                  <Button className="w-full" type="submit">
+                    Login
+                  </Button>
+
+                  {/* <Button variant="outline" className="w-full"> */}
+                  {/*   Login with Google */}
+                  {/* </Button> */}
+                </div>
+              </div>
+
+              <div className="mt-4 text-center text-sm">
+                Don&apos;t have account?{" "}
+                <Link to="/signup" className="underline underline-offset-4">
+                  Signup
+                </Link>
+              </div>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
   );
 }
