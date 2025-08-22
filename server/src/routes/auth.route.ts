7b3f207f127896a1d79406927a8d850c56313464
p.andhika <p.andhika@op.iitg.ac.in>
2025-08-22 21:12:38 +0700

    refactor: convert auth route to function for app injection

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,30 +1,34 @@
+import { AppApi } from "@/lib/types";
 import { getSupabase } from "@/middlewares/auth.middleware";
-import { Hono } from "hono";
 
-export const authRoute = new Hono()
-  .post("/signin", async (c) => {
-    const body = await c.req.json();
-    const supabase = getSupabase(c);
-    const response = await supabase.auth.signInWithPassword({
-      email: body.email,
-      password: body.password,
+export default function authRoute(app: AppApi) {
+  app
+    .post("/signin", async (c) => {
+      const body = await c.req.json();
+      const supabase = getSupabase(c);
+      const response = await supabase.auth.signInWithPassword({
+        email: body.email,
+        password: body.password,
+      });
+
+      return c.json(response);
+    })
+
+    .get("/signout", async (c) => {
+      const supabase = getSupabase(c);
+      await supabase.auth.signOut();
+
+      return c.json({
+        message: "Signed out server-side!",
+      });
+    })
+
+    .get("/user", async (c) => {
+      const supabase = getSupabase(c);
+      const { data } = await supabase.auth.getUser();
+
+      return c.json(data);
     });
 
-    return c.json(response);
-  })
-
-  .get("/signout", async (c) => {
-    const supabase = getSupabase(c);
-    await supabase.auth.signOut();
-
-    return c.json({
-      message: "Signed out server-side!",
-    });
-  })
-
-  .get("/user", async (c) => {
-    const supabase = getSupabase(c);
-    const { data } = await supabase.auth.getUser();
-
-    return c.json(data);
-  });
+  return app;
+}
