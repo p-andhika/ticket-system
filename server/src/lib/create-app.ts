267e2341e6ee6581b15da22d6cc0692c28ce0749
p.andhika <p.andhika@op.iitg.ac.in>
2025-08-22 20:50:05 +0700

    refactor: modularize app creation logic

---
+++ server/src/lib/create-app.ts
@@ -0,0 +1,27 @@
+import { AppBindings } from "@/lib/types";
+import { supabaseMiddleware } from "@/middlewares/auth.middleware";
+import { logger } from "@/middlewares/logger.middleware";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { notFound, onError } from "stoker/middlewares";
+
+export default function createApp() {
+  const app = new Hono<AppBindings>({
+    strict: false,
+  });
+
+  app.use(
+    "*",
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+    }),
+  );
+  app.use("*", supabaseMiddleware());
+  app.use(logger());
+
+  app.notFound(notFound);
+  app.onError(onError);
+
+  return app;
+}
