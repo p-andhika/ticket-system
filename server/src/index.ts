c6a34bd43215f16fbbd077fee8341b5ee4a265c6
p.andhika <p.andhika@op.iitg.ac.in>
2025-08-22 19:27:28 +0700

    feat: integrate stoker middleware and update paths

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,13 @@
+import { supabaseMiddleware } from "@/middleware/auth.middleware";
+import { authRoute } from "@/routes/auth";
 import "@dotenvx/dotenvx/config";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { logger } from "hono/logger";
-import { supabaseMiddleware } from "./middleware/auth.middleware";
-import { authRoute } from "./routes/auth";
+import { notFound, onError } from "stoker/middlewares";
 
 const app = new Hono();
+
 app.use(logger());
 app.use(
   "*",
@@ -17,5 +19,8 @@ app.use(
 app.use("*", supabaseMiddleware());
 
 app.route("/api/v1/auth", authRoute);
+
+app.notFound(notFound);
+app.onError(onError);
 
 export default app;
