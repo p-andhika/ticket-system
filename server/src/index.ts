a8697e33bbd5f151a06ebd80199ac6b1328e520b
Andhika Prakasiwi <p.andhika@op.iitg.ac.in>
2025-11-04 14:21:02 +0700

    refactor: replace Supabase client with custom API

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,51 +1,43 @@
+import "@dotenvx/dotenvx/config";
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { getSupabase, supabaseMiddleware } from "./middleware/auth.middleware";
 
 const app = new Hono();
+app.use(
+  "*",
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use("*", supabaseMiddleware());
 
-app.get("/api/user", async (c) => {
+app.get("/api/v1/user", async (c) => {
   const supabase = getSupabase(c);
   const { data } = await supabase.auth.getUser();
 
-  if (!data?.user) {
-    return c.json({
-      message: "You are not logged in.",
-    });
-  }
-
-  return c.json({
-    message: "You are logged in!",
-    userId: data.user,
-  });
+  return c.json(data);
 });
 
-app.get("/signin", async (c) => {
+app.post("/api/v1/signin", async (c) => {
+  const body = await c.req.json();
   const supabase = getSupabase(c);
-  await supabase.auth.signInWithPassword({
-    email: "test@example.com",
-    password: "superman99",
+  const response = await supabase.auth.signInWithPassword({
+    email: body.email,
+    password: body.password,
   });
 
-  return c.json({
-    message: "Signed in server-side!",
-  });
+  return c.json(response);
 });
 
-app.get("/signout", async (c) => {
+app.get("/api/v1/signout", async (c) => {
   const supabase = getSupabase(c);
   await supabase.auth.signOut();
 
   return c.json({
     message: "Signed out server-side!",
   });
-});
-
-app.get("/countries", async (c) => {
-  const supabase = getSupabase(c);
-  const { data, error } = await supabase.from("countries").select("*");
-  if (error) console.log(error);
-  return c.json(data);
 });
 
 export default app;
