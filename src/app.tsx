9fe2ed61fbd1e9a3db12bfe0108fc95ef5db0363
Andhika Prakasiwi <p.andhika@op.iitg.ac.in>
2025-11-04 14:19:02 +0700

    refactor: modularize app setup and improve login error handling

---
+++ src/app.tsx
@@ -0,0 +1,47 @@
+import { useAuth } from "@/hooks/use-auth";
+import { routeTree } from "@/routeTree.gen";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // With SSR, we usually want to set some default staleTime
+      // above 0 to avoid refetching immediately on the client
+      // staleTime: 60 * 1000,
+      refetchOnWindowFocus: false,
+      retry: false,
+      networkMode: "always",
+    },
+  },
+});
+
+// Set up a Router instance
+const router = createRouter({
+  routeTree,
+  context: { auth: null, queryClient },
+  defaultPreload: "intent",
+  // Since we're using React Query, we don't want loader calls to ever be stale
+  // This will ensure that the loader is always called when the route is preloaded or visited
+  defaultPreloadStaleTime: 0,
+  defaultStructuralSharing: true,
+  scrollRestoration: true,
+});
+
+// Register things for typesafety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+function App() {
+  const session = useAuth();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} context={{ auth: session }} />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
