9ab8a16b5a6fd3988d5ba607b9e50ea29eae63d3
Andhika Prakasiwi <p.andhika@op.iitg.ac.in>
2025-11-04 14:21:02 +0700

    refactor: modularize app creation logic

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,37 +1,8 @@
-import { supabaseMiddleware } from "@/middlewares/auth.middleware";
-import { logger } from "@/middlewares/logger.middleware";
+import createApp from "@/lib/create-app";
 import { authRoute } from "@/routes/auth";
-import "@dotenvx/dotenvx/config";
-import { Hono } from "hono";
-import { PinoLogger } from "hono-pino";
-import { cors } from "hono/cors";
-import { notFound, onError } from "stoker/middlewares";
 
-interface AppBindings {
-  Variables: {
-    logger: PinoLogger;
-  };
-}
-
-const app = new Hono<AppBindings>();
-
-app.use(
-  "*",
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  }),
-);
-app.use("*", supabaseMiddleware());
-app.use(logger());
-
-app.get("/error", () => {
-  throw new Error("Something went wrong!");
-});
+const app = createApp();
 
 app.route("/api/v1/auth", authRoute);
-
-app.notFound(notFound);
-app.onError(onError);
 
 export default app;
