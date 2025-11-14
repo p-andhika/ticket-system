a2aa577a298d9c8bc7d2ea6da9f12b334d2aa0b9
Andhika Prakasiwi <p.andhika@op.iitg.ac.in>
2025-11-14 12:28:07 +0700

    feat(auth): add forgot password form and route

--- client/src/routes/_public/forgot-password.tsx
+++ client/src/routes/_public/forgot-password.tsx
@@ -1,9 +1,45 @@
-import { createFileRoute } from "@tanstack/react-router";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { ForgotPasswordForm } from "@/domain/auth/view/components/forgot-password-form";
+import { createFileRoute, Link } from "@tanstack/react-router";
+import { GalleryVerticalEnd } from "lucide-react";
 
 export const Route = createFileRoute("/_public/forgot-password")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/(auth)/forgot-password"!</div>;
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Link
+          to="/"
+          className="flex items-center gap-2 self-center font-medium"
+        >
+          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+            <GalleryVerticalEnd className="size-4" />
+          </div>
+          Acme Inc
+        </Link>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>Reset your password</CardTitle>
+            <CardDescription>
+              Enter your user account's verified email address and we will send
+              you a password reset link
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <ForgotPasswordForm />
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 }
